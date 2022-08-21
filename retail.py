b16f4635daa14ac19ffc6b0b9848c0a534c62e97
crist2201 <cristiansalazar822@gmail.com>
2022-08-20 21:10:21 -0400

    Add data analysis and charts

--- retail.py
+++ retail.py
@@ -1,4 +1,60 @@
 import pandas as pd
 import streamlit as st
 
-st.title("RETAIL OPERATION")
+# Datasets
+df_expected = pd.read_csv(
+    "https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+df_counted = pd.read_csv(
+    "https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+
+my_cols_selected = ["Retail_Product_Color",
+                    "Retail_Product_Level1",
+                    "Retail_Product_Level1Name",
+                    "Retail_Product_Level2Name",
+                    "Retail_Product_Level3Name",
+                    "Retail_Product_Level4Name",
+                    "Retail_Product_Name",
+                    "Retail_Product_SKU",
+                    "Retail_Product_Size",
+                    "Retail_Product_Style",
+                    "Retail_SOHQTY"]
+
+
+# Tratamiento del dataset counted
+df_counted = df_counted.drop_duplicates("RFID")
+
+df_B = df_counted.groupby("Retail_Product_SKU").count(
+)[["RFID"]].reset_index().rename(columns={"RFID": "Retail_CCQTY"})
+
+# Tratamiento del dataset expected
+df_A = df_expected[my_cols_selected]
+
+# Relacionar ambos datasets tratados
+df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU",
+                          right_on="Retail_Product_SKU", indicator=True)
+df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0)
+df_discrepancy["Retail_CCQTY"] = df_discrepancy["Retail_CCQTY"].astype(int)
+df_discrepancy["Retail_SOHQTY"] = df_discrepancy["Retail_SOHQTY"].fillna(
+    0).astype(int)
+
+df_discrepancy["Diff"] = df_discrepancy["Retail_CCQTY"] - \
+    df_discrepancy["Retail_SOHQTY"]
+df_discrepancy.loc[df_discrepancy["Diff"] < 0,
+                   "Unders"] = df_discrepancy["Diff"] * (-1)
+df_discrepancy["Unders"] = df_discrepancy["Unders"].fillna(0).astype(int)
+
+
+# Cantidades totales
+sqty = df_discrepancy['Retail_SOHQTY'].sum()
+cqty = df_discrepancy['Retail_CCQTY'].sum()
+
+# Diferencia total
+diff = cqty-sqty
+
+
+container = st.container()
+container.title("INVENTORY DISCREPANCY")
+val1, val2, val3 = container.columns(3)
+val1.metric(label="SOH INVENTORY", variable=sqty)
+val2.metric(label="MOJIX INVENTORY", variable=cqty)
+val3.metric(label="DIFFERENCE", variable=diff)
